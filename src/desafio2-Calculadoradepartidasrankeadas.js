9a974e0415cd951a6d784ecc839971c723cada9e
flaviasouzza <flavia.souza.lvi@gmail.com>
2025-01-15 16:29:56 -0300

    Desafio Concluído

--- src/desafio2-Calculadoradepartidasrankeadas.js
+++ src/desafio2-Calculadoradepartidasrankeadas.js
@@ -1,15 +1,3 @@
-/* - *O Que deve ser utilizado**
-- Variáveis
-- Operadores
-- Laços de repetição
-- Estruturas de decisões
-- Funções
-
-Crie uma função que recebe como parâmetro a quantidade de vitórias e derrotas de um jogador,
-
-depois disso retorne o resultado para uma variável, o saldo de Rankeadas deve ser 
-feito através do calculo (vitórias - derrotas) */
-
 const prompt = require("prompt-sync")();
 while(true) {
 let name = prompt("Enter the hero's name: ");
